fd55218672c3d70d85ef579d8c040861dee43466
wsg011 <wsg20110828@163.com>
2020-11-18 15:04:47 +0800

    add sakt for 0.77

---
+++ sakt/dataset.py
@@ -0,0 +1,83 @@
+import itertools
+import numpy as np
+import pandas as pd
+
+import torch
+from torch.utils.data import Dataset
+
+
+class SAKTDataset(Dataset):
+    def __init__(self, fn, n_skill, max_seq=100, split="train"):
+        super(SAKTDataset, self).__init__()
+        self.n_skill = n_skill
+        self.max_seq = max_seq
+        self.split = split
+
+        self.user_ids = []
+        self.samples = []
+        with open(fn, "r") as csv_f:
+            for student_id, q, qa in itertools.zip_longest(*[csv_f] * 3):
+                student_id = int(student_id.strip())
+                q = [int(x) for x in q.strip().split(",") if x]
+                qa = [int(x) for x in qa.strip().split(",") if x]
+
+                assert len(q) == len(qa)
+
+                self.user_ids.append(student_id)
+                self.samples.append((q, qa))
+
+    def __len__(self):
+        return len(self.user_ids)
+
+    def __getitem__(self, index):
+        """
+        get sakt model input
+        args:
+            index
+        returns:
+            x:
+            question_id:
+            target_id:
+            correct
+        """
+        user_id = self.user_ids[index]
+        q_, qa_ = self.samples[index]
+        seq_len = len(q_)
+
+        q = np.zeros(self.max_seq, dtype=int)
+        qa = np.zeros(self.max_seq, dtype=int)
+        if seq_len >= self.max_seq:
+            q[:] = q_[-self.max_seq:]
+            qa[:] = qa_[-self.max_seq:]
+        else:
+            q[-seq_len:] = q_
+            qa[-seq_len:] = qa_
+
+        # target_id = np.array([q[-1]])
+        # label = np.array([qa[-1]])
+        if self.split == "train":
+            target_id = q[1:]
+            label = qa[1:]
+        else:
+            target_id = np.array([q[-1]])
+            label = np.array([qa[-1]])
+    
+        x = q[:-1].copy()
+        x += (qa[:-1] == 1) * self.n_skill
+
+        q = q[:-1]
+
+        assert x.shape == q.shape
+        assert target_id.shape == label.shape
+
+        return x, q, target_id, label 
+
+
+if __name__ == "__main__":
+    dataset = SAKTDataset("../data/ASSISTments2009/train.csv", n_skill=124)
+
+    x, q, target_id, label = dataset.__getitem__(10)
+    print(x)
+    print(q)
+    print(target_id)
+    print(label)
---
+++ sakt/model/sakt.py
@@ -0,0 +1,83 @@
+import torch
+import torch.nn as nn
+
+
+class FFN(nn.Module):
+    def __init__(self, state_size=200):
+        super(FFN, self).__init__()
+        self.state_size = state_size
+
+        self.lr1 = nn.Linear(state_size, state_size)
+        self.relu = nn.ReLU()
+        self.lr2 = nn.Linear(state_size, state_size)
+        self.dropout = nn.Dropout(0.2)
+    
+    def forward(self, x):
+        x = self.lr1(x)
+        x = self.relu(x)
+        x = self.lr2(x)
+        return self.dropout(x)
+
+
+class SAKTModel(nn.Module):
+    def __init__(self, n_skill, max_seq=200, embed_dim=200):
+        super(SAKTModel, self).__init__()
+        self.n_skill = n_skill
+        self.embed_dim = embed_dim
+
+        self.embedding = nn.Embedding(2*n_skill+1, embed_dim)
+        self.pos_embedding = nn.Embedding(max_seq-1, embed_dim)
+        self.e_embedding = nn.Embedding(n_skill+1, embed_dim)
+
+        self.multi_att = nn.MultiheadAttention(embed_dim=embed_dim, num_heads=8, dropout=0.2)
+
+        self.dropout = nn.Dropout(0.2)
+        self.layer_normal = nn.LayerNorm(embed_dim) 
+
+        self.ffn = FFN(embed_dim)
+        self.pred = nn.Linear(embed_dim, n_skill)
+    
+    def forward(self, x, question_ids, e):
+        device = x.device
+        x = self.embedding(x)
+        pos_id = torch.arange(x.size(1)).unsqueeze(0).to(device)
+
+        pos_x = self.pos_embedding(pos_id)
+        x = x + pos_x
+        # x = self.layer_normal(x)
+        res = x
+
+        e = self.e_embedding(e)
+
+        x = x.permute(1, 0, 2) # x: [bs, s_len, embed] => [s_len, bs, embed]
+        e = e.permute(1, 0, 2)
+        att_output, att_weight = self.multi_att(e, x, x)
+        att_output = att_output.permute(1, 0, 2) # att_output: [s_len, bs, embed] => [bs, s_len, embed]
+        # print(att_output.shape, att_weight.shape)
+
+        x = self.layer_normal(att_output)
+        x = self.ffn(att_output)
+        x = x + res
+        x = self.layer_normal(x)
+        x = self.dropout(x)
+        x = self.pred(x)
+
+        return x, att_weight
+
+
+if __name__ == "__main__":
+    q = torch.randint(0, 100, size=(1, 6))
+    qa = torch.randint(0, 2, size=(1, 6))
+    print(q, qa)
+
+    x = q[:, :-1].clone()
+    print(x)
+    x += (qa[:, :-1] == 1) * 100
+    print(x)
+    e = q[:, 1:].clone()
+    question_ids = q[:, :-1].clone()
+
+    model = SAKTModel(n_skill=100)
+    ouput, att_weight = model(x, question_ids, e)
+    print(ouput.shape)
+    print(att_weight.shape)
